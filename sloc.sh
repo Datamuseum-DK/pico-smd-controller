fcd71cad3c05524f1dfbf19d618048ca8627336f
Anders Kaare Straadt <anders@straadt.dk>
2023-07-19 00:00:17 +0200

    controller sloc

--- sloc.sh
+++ sloc.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
+echo -n "controller: "
+cat $(git ls-files '*.c' '*.h' '*.pio' | grep -vF frontend_graphical | grep -vF print_pin_config_html.c) | wc -l
+echo "-----"
 F0=frontend_graphical
 wc -l $(git ls-files '*.c' '*.h' '*.cpp' '*.pio' | grep -vF $F0/imgui | grep -vF $F0/imstb_ | grep -vF $F0/imconfig.h | grep -vF stb_ds.h)
