8cb652fda0513a7a8b8890d2922a88c72fe75624
Anders Kaare Straadt <anders@straadt.dk>
2023-07-20 22:22:22 +0200

    missing rename

--- base64.h
+++ base64.h
@@ -1,7 +1,7 @@
-#ifndef B64_H
+#ifndef BASE64_H
 
 char* base64_encode(char* output, uint8_t* input, int n_bytes);
 uint8_t* base64_decode_line(uint8_t* output, char* line);
 
-#define B64_H
+#define BASE64_H
 #endif
