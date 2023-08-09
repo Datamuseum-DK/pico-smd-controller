6ba39a7a0507d70be0de4782fb480328fbd50953
Anders Kaare Straadt <anders@straadt.dk>
2023-08-09 23:35:37 +0200

    output useful debugging info

--- loopback_verify.c
+++ loopback_verify.c
@@ -41,11 +41,20 @@ int main(int argc, char** argv)
 			if (err == 0) first_error_index = i;
 			last_error_index = i;
 			err = 1;
+			char bits_expected[9];
+			char bits_actual[9];
+			for (int b = 0; b < 8; b++) {
+				bits_expected[b] = ((expected[i] >> (7-b)) & 1) ? '1' : '0';
+				bits_actual[b]   = ((actual[i]   >> (7-b)) & 1) ? '1' : '0';
+			}
+			bits_expected[8] = 0;
+			bits_actual[8] = 0;
+			printf("at position 0x%.4x: expected 0x%.2x (%s); actual was 0x%.2x (%s)\n", i, expected[i], bits_expected, actual[i], bits_actual);
 		}
 	}
 
 	if (err) {
-		fprintf(stderr, "%s: ERRORS between offset %d (0x%x) and %d (0x%x)\n", path, first_error_index, first_error_index, last_error_index, last_error_index);
+		printf("%s: ERRORS between offset %d (0x%x) and %d (0x%x)\n", path, first_error_index, first_error_index, last_error_index, last_error_index);
 	} else {
 		printf("%s: OK!\n", path);
 	}
