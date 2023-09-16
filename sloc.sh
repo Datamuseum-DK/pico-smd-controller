6928d4e396319cbb55d437ca9a96932c0db45899
Anders Kaare Straadt <anders@straadt.dk>
2023-09-16 16:33:02 +0200

    bad sloc

--- sloc.sh
+++ sloc.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
+cd $(dirname $0)
 echo -n "controller: "
-cat $(git ls-files '*.c' '*.h' '*.pio' | grep -vF frontend_graphical | grep -vF print_pin_config_html.c) | wc -l
+cat $(git ls-files '*.c' '*.h' '*.pio' | grep -vF / ) | wc -l
 echo "-----"
 F0=frontend_graphical
-wc -l $(git ls-files '*.c' '*.h' '*.cpp' '*.pio' | grep -vF $F0/imgui | grep -vF $F0/imstb_ | grep -vF $F0/imconfig.h | grep -vF stb_ds.h)
+wc -l $(git ls-files '*.c' '*.h' '*.cpp' '*.pio' | grep -vF $F0/imgui | grep -vF $F0/imstb_ | grep -vF $F0/imconfig.h | grep -vF $F0/stb_ds.h)
