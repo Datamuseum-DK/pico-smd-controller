77fce81bf3baaf78bd9ce3fffabcb4be183028a9
Anders Kaare Straadt <anders@straadt.dk>
2023-12-22 18:49:45 +0100

    fixing things (?)
     - fixed incorrect DMA channel setup for the "pull words"; it had the src/dst
       addresses swapped
    
     - disabled "autopull" for the OSR since the PIO program does its own PULLs.
       maybe it would be possible to get rid of 4 PULL instructions by enabling
       autopull, but there's one PULL I can't get rid of (because autopull is a
       feature for the OUT instruction, and MOV X,OSR can't do autopulls). however,
       I'm a bit scared of mixing it. however, it's an easy thing to test once the
       program is otherwise working? just change
         sm_config_set_out_shift(&cfg, /*shift_right=*/false, /*autopull=*/false, /*pull_threshold=*/32);
       to
         sm_config_set_out_shift(&cfg, /*shift_right=*/false, /*autopull=*/true,  /*pull_threshold=*/32);
       and remove the "pull" before every "out pins, 32"
    
     - added coded to setup PINDIRS properly; it needs a "1" for every output used
    
     - pio_gpio_init() wasn't called properly; it sets the "GPIO function" to
       PIO0/1. this is also not required for inputs, only outputs
    
     - narrowed the output pins the PIO can drive to BIT1,2,3,7,8; TAG3 and
       UNIT_SELECT_TAG are driven by software (they shouldn't change for the
       duration of the read)

--- cr8044read.c
+++ cr8044read.c
@@ -4,6 +4,7 @@
 #include "cr8044read.pio.h"
 #include "pin_config.h"
 #include "base.h"
+#include "controller_protocol.h"
 
 _Static_assert(cr8044read_READ_DATA == GPIO_READ_DATA);
 _Static_assert(cr8044read_READ_CLOCK == GPIO_READ_CLOCK);
@@ -24,12 +25,18 @@ static inline void cr8044read_program_init(PIO pio, uint sm, uint offset)
 {
 	pio_sm_config cfg = cr8044read_program_get_default_config(offset);
 	sm_config_set_in_pins(&cfg, GPIO_READ_DATA);
-	pio_gpio_init(pio, 0);
-	//
+	sm_config_set_out_pins(&cfg, 0, 32);
+
+	pio_sm_set_consecutive_pindirs(pio, sm, GPIO_BIT1, /*pin_count=*/1, /*is_out=*/true);
+	pio_sm_set_consecutive_pindirs(pio, sm, GPIO_BIT2, /*pin_count=*/1, /*is_out=*/true);
+	pio_sm_set_consecutive_pindirs(pio, sm, GPIO_BIT3, /*pin_count=*/1, /*is_out=*/true);
+	pio_sm_set_consecutive_pindirs(pio, sm, GPIO_BIT7, /*pin_count=*/1, /*is_out=*/true);
+	pio_sm_set_consecutive_pindirs(pio, sm, GPIO_BIT8, /*pin_count=*/1, /*is_out=*/true);
+
 	sm_config_set_in_shift(&cfg, /*shift_right=*/true, /*autopush=*/true, /*push_threshold=*/32);
-	sm_config_set_out_shift(&cfg, /*shift_right=*/false, /*autopull=*/true, /*pull_threshold=*/32);
+	sm_config_set_out_shift(&cfg, /*shift_right=*/false, /*autopull=*/false, /*pull_threshold=*/32);
 	sm_config_set_fifo_join(&cfg, PIO_FIFO_JOIN_NONE);
-	//
+
 	pio_sm_init(pio, sm, offset, &cfg);
 }
 //
@@ -51,9 +58,8 @@ void cr8044read_init(PIO _pio, uint _dma_channel, uint _dma_channel2)
 
 void cr8044read_prep(unsigned extra_read_enable_bits)
 {
-	const unsigned all_gpio = GPIO_TAG3 | GPIO_UNIT_SELECT_TAG; // must be high even during read disable
-	const unsigned read_enable_gpio  = all_gpio | GPIO_BIT1 | extra_read_enable_bits;
-	const unsigned read_disable_gpio = all_gpio;
+	const unsigned read_enable_gpio = GPIO_BIT1 | extra_read_enable_bits;
+	const unsigned read_disable_gpio = 0;
 	unsigned* wp = pull_words;
 	for (int i0 = 0; i0 < CR8044READ_N_SECTORS; i0++) {
 		*(wp++) = read_enable_gpio;
@@ -69,39 +75,58 @@ void cr8044read_execute(uint8_t* dst)
 {
 	pio_sm_set_enabled(pio, sm, false);
 
+	// PIO must be able to drive these pins. these calls are like calling
+	// gpio_set_function() with the corresponding pio (PIO0/1)
+	pio_gpio_init(pio, GPIO_BIT1); // read gate
+	pio_gpio_init(pio, GPIO_BIT2); // servo offset positive
+	pio_gpio_init(pio, GPIO_BIT3); // servo offset negative
+	pio_gpio_init(pio, GPIO_BIT7); // data strobe early
+	pio_gpio_init(pio, GPIO_BIT8); // data strobe late
+
 	pio_sm_clear_fifos(pio, sm);
 	pio_sm_restart(pio, sm);
 
-	dma_channel_config dma_channel_cfg = dma_channel_get_default_config(dma_channel);
-	channel_config_set_read_increment(&dma_channel_cfg,  false);
-	channel_config_set_write_increment(&dma_channel_cfg, true);
-	channel_config_set_dreq(&dma_channel_cfg, pio_get_dreq(pio, sm, false));
-
-	dma_channel_config dma_channel2_cfg = dma_channel_get_default_config(dma_channel2);
-	channel_config_set_read_increment(&dma_channel2_cfg,  true);
-	channel_config_set_write_increment(&dma_channel2_cfg, false);
-	channel_config_set_dreq(&dma_channel2_cfg, pio_get_dreq(pio, sm, true));
-
-	dma_channel_configure(
-		dma_channel2,
-		&dma_channel2_cfg,
-		pull_words,
-		&pio->txf[sm],             // write to PIO TX FIFO
-		N_PULL_WORDS,
-		true // start now!
-	);
-
 	const int word_32bit_count = (CR8044READ_BYTES_TOTAL + 3) >> 2;
-	dma_channel_configure(
-		dma_channel,
-		&dma_channel_cfg,
-		dst,
-		&pio->rxf[sm],             // read from PIO RX FIFO
-		word_32bit_count,
-		true // start now!
-	);
+
+	{
+		dma_channel_config dma_channel_cfg = dma_channel_get_default_config(dma_channel);
+		channel_config_set_read_increment(&dma_channel_cfg,  false);
+		channel_config_set_write_increment(&dma_channel_cfg, true);
+		channel_config_set_dreq(&dma_channel_cfg, pio_get_dreq(pio, sm, false));
+		dma_channel_configure(
+			dma_channel,
+			&dma_channel_cfg,
+			dst,
+			&pio->rxf[sm],             // read from PIO RX FIFO
+			word_32bit_count,
+			true // start now!
+		);
+	}
+
+	{
+		dma_channel_config dma_channel2_cfg = dma_channel_get_default_config(dma_channel2);
+		channel_config_set_read_increment(&dma_channel2_cfg,  true);
+		channel_config_set_write_increment(&dma_channel2_cfg, false);
+		channel_config_set_dreq(&dma_channel2_cfg, pio_get_dreq(pio, sm, true));
+		dma_channel_configure(
+			dma_channel2,
+			&dma_channel2_cfg,
+			&pio->txf[sm],             // write to PIO TX FIFO
+			pull_words,
+			N_PULL_WORDS,
+			true // start now!
+		);
+	}
 
 	pio_sm_set_enabled(pio, sm, true);
 	while (dma_channel_is_busy(dma_channel)) {};
 	pio_sm_set_enabled(pio, sm, false);
+
+	// reset the effect of calling pio_gpio_init() above so that software
+	// can drive these pins again
+	gpio_set_function(GPIO_BIT1, GPIO_FUNC_SIO);
+	gpio_set_function(GPIO_BIT2, GPIO_FUNC_SIO);
+	gpio_set_function(GPIO_BIT3, GPIO_FUNC_SIO);
+	gpio_set_function(GPIO_BIT7, GPIO_FUNC_SIO);
+	gpio_set_function(GPIO_BIT8, GPIO_FUNC_SIO);
 }
